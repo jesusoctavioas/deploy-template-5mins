c574d73b53c762afc9bc6a62d3239333a92b063b
Sri <srirangan@gmail.com>
2020-11-25 16:32:10 +0100

    Use correct container path to pull and run

--- deploy.sh
+++ deploy.sh
@@ -55,7 +55,7 @@ ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i private_key.p
     sudo amazon-linux-extras install docker
     sudo service docker start
 
-    sudo docker login --username $CI_REGISTRY_USER --password $CI_REGISTRY_PASSWORD $CI_REGISTRY_IMAGE
+    sudo docker login --username $CI_REGISTRY_USER --password $CI_REGISTRY_PASSWORD $CI_REGISTRY_IMAGE/$CI_COMMIT_REF_SLUG
  "
 
 # stop and remove all existing containers
@@ -69,7 +69,7 @@ ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i private_key.p
 # DB_INITIALIZE
 # DB_MIGRATE
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i private_key.pem ec2-user@"$(cat public_ip.txt)" "
-    sudo docker pull $CI_APPLICATION_TAG
+    sudo docker pull $CI_REGISTRY_IMAGE/$CI_COMMIT_REF_SLUG:$CI_APPLICATION_TAG
 
     sudo docker run --name container_webapp                                 \
         -e AWS_ACCESS_KEY=$AWS_ACCESS_KEY                                   \
@@ -86,7 +86,7 @@ ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i private_key.p
         -e S3_BUCKET_REGIONAL_DOMAIN=$S3_BUCKET_REGIONAL_DOMAIN             \
         -d                                                                  \
         -p 80:$WEBAPP_PORT                                                  \
-        $CI_APPLICATION_TAG
+        $CI_REGISTRY_IMAGE/$CI_COMMIT_REF_SLUG:$CI_APPLICATION_TAG
 
     echo \"DB_INITIALIZE_REPEAT: $DB_INITIALIZE_REPEAT\"
     if [ \"$DB_INITIALIZE_REPEAT\" == \"True\" ]; then
