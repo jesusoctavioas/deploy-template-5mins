3ee71aa771ac93a2b230b1136c8f0ff63657e264
Sri <srirangan@gmail.com>
2020-12-02 21:33:03 +0100

    Docker runs webapp and binds port 8000

--- deploy.sh
+++ deploy.sh
@@ -85,7 +85,7 @@ ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i private_key.p
         $GL_VARs                                                            \
         -e S3_BUCKET_REGIONAL_DOMAIN=$S3_BUCKET_REGIONAL_DOMAIN             \
         -d                                                                  \
-        -p 80:$WEBAPP_PORT                                                  \
+        -p 8000:$WEBAPP_PORT                                                  \
         $CI_REGISTRY_IMAGE/$CI_COMMIT_REF_SLUG:$CI_APPLICATION_TAG
 
     echo \"DB_INITIALIZE_REPEAT: $DB_INITIALIZE_REPEAT\"
