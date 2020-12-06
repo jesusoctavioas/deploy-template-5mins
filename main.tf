6455906184ac18a792ed3813bf866b337918fbcb
Sri <srirangan@gmail.com>
2020-12-06 13:04:08 +0100

    Expose port 8000 for

--- main.tf
+++ main.tf
@@ -85,6 +85,14 @@ resource "aws_security_group" "five_minute_public" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        description = "HTTP"
+        from_port = 8000
+        to_port = 8000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         from_port = 0
         to_port = 0
