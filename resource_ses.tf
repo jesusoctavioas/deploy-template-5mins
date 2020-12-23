2a87712d054c7833050fd288ad457f78a563765c
Sri <srirangan@gmail.com>
2020-12-23 10:29:06 +0100

    AWS SES resources are created only when SMTP_FROM is defined

--- resource_ses.tf
+++ resource_ses.tf
@@ -1,18 +1,22 @@
 resource "aws_ses_email_identity" "email_identity" {
-  email = var.SMTP_FROM != "" ? var.SMTP_FROM : var.SERVICE_DESK_EMAIL
+  count = var.SMTP_FROM != "" ? 1 : 0
+  email = var.SMTP_FROM != "" ? var.SMTP_FROM : ""
 }
 
 resource "aws_iam_user" "smtp_user" {
+  count = var.SMTP_FROM != "" ? 1 : 0
   name = "smtp_user_${var.SHORT_ENVIRONMENT_NAME}"
   tags = local.common_tags
 }
 
 resource "aws_iam_access_key" "smtp_user_access_key" {
-  user = aws_iam_user.smtp_user.name
+  count = var.SMTP_FROM != "" ? 1 : 0
+  user = aws_iam_user.smtp_user[0].name
 }
 
 resource "aws_iam_user_policy" "smtp_user_policy" {
-  user = aws_iam_user.smtp_user.name
+  count = var.SMTP_FROM != "" ? 1 : 0
+  user = aws_iam_user.smtp_user[0].name
   policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -24,10 +28,10 @@ POLICY
 # Output
 
 output "smtp_user" {
-  value = aws_iam_access_key.smtp_user_access_key.id
+  value = var.SMTP_FROM != "" ? aws_iam_access_key.smtp_user_access_key[0].id : ""
 }
 
 output "smtp_password" {
-  value = aws_iam_access_key.smtp_user_access_key.ses_smtp_password_v4
+  value = var.SMTP_FROM != "" ? aws_iam_access_key.smtp_user_access_key[0].ses_smtp_password_v4 : ""
   sensitive = true
 }
