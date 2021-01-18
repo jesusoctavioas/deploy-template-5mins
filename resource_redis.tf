2a1c0a4a29ac83ba77225bea51e30a490ad08cb9
Sri <srirangan@gmail.com>
2021-01-18 22:23:03 +0100

    Introduce ENV_NAME in cluster_id

--- resource_redis.tf
+++ resource_redis.tf
@@ -1,6 +1,6 @@
 resource "aws_elasticache_cluster" "redis" {
   count = var.REDIS_NODE_TYPE != "" ? 1 : 0
-  cluster_id = "redis-cluster"
+  cluster_id = "redis-cluster-${var.SHORT_ENVIRONMENT_NAME}"
   engine = "redis"
   node_type = var.REDIS_NODE_TYPE
   num_cache_nodes = 1
